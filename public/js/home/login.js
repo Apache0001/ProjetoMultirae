dd3c2b512b342fc5725898d98da2403f59e2d383
Pablo Oliveira <68168336+Apache0001@users.noreply.github.com>
2021-01-13 18:24:46 -0300

    Terceiro Commit: Add cadastro

---
+++ public/js/home/login.js
@@ -0,0 +1,18 @@
+// JQuery
+//
+//
+$(document).ready(function(){
+        $('#entrar').on('click', () =>{
+            $('#telaLogin').css('display', 'block');
+            $('#telaLogin').css('position', 'absolute');
+            $('#telaLogin').css('z-index', '2');
+
+
+    })
+
+        $('#icoIMG').on('click', () =>{
+            $('#telaLogin').css('display','none')
+        })
+})
+
+
