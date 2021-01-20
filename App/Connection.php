b175092e13d07dcfaca1a1aa314c64b5fd3796d9
Pablo Oliveira Mesquita <pablo_omesquita@hotmail.com>
2021-01-20 20:15:56 -0300

    Update Connection.php

--- App/Connection.php
+++ App/Connection.php
@@ -4,7 +4,7 @@ class Connection{
         public function getDb(){
             try{
                 
-                $conn = new \PDO("mysql:host=localhost;dbname=recodeprojeto;charset=utf8","root","");
+                $conn = new \PDO("mysql:host=localhost;dbname=id15784893_recodeprojeto;charset=utf8","root",">geQxTtJwMP-f|=9");
                 return $conn;
             }
             catch(\PDOException $erro){
